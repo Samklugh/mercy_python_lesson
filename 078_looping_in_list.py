72d4234845d558ecaf990df74fd7e94aac9f01ce
Samson Oladimeji Odunaiya <102425294+Samklugh@users.noreply.github.com>
2026-02-21 08:51:30 +0000

    loping in list

---
+++ 078_looping_in_list.py
@@ -0,0 +1,20 @@
+# Looping in list 
+
+# 1. Looping through a list using for loop:
+
+# name=["John", "Jane", "Doe", ["Mercy", "Grace"]]
+# 
+# for elements in name:
+    # print(elements)
+
+# name=["John", "Jane", "Doe", ["Mercy", "Grace"]]
+# 
+# for i in range(len(name)):
+    # print(name[i])
+# 
+# 2. Looping through a list using while loop:
+# name=["John", "Jane", "Doe", ["Mercy", "Grace"]]
+# i=0
+# while i < len(name):
+    # print(name[i])
+    # i+=1
