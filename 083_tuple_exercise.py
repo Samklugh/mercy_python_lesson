a249a1cfa4f200b411686790d750959a1da4f3fb
Samson Oladimeji Odunaiya <102425294+Samklugh@users.noreply.github.com>
2026-03-05 13:16:26 +0000

    TUPLE EXERCISES

--- 083_tuple_exercise.py
+++ 083_tuple_exercise.py
@@ -1,32 +1,52 @@
 # Question 1:
 
-fruits= ("apple", "banana", "cherry", "mango")
+# fruits= ("apple", "banana", "cherry", "mango")
 
-print(fruits)
+# print(fruits)
 
 # first item
-print(fruits[0])
+# print(fruits[0])
 # last item
-print(fruits[-1])
+# print(fruits[-1])
 
 
 # Question 2:
-numbers= (10, 20, 30, 40, 50)
-print(len(numbers))
-print(type(numbers))
+# numbers= (10, 20, 30, 40, 50)
+# print(len(numbers))
+# print(type(numbers))
 
 # Question 3:
-colors=("red", "blue", "green", "yellow", "purple")
-print(colors[:3])
-print(colors[-2:])
+# colors=("red", "blue", "green", "yellow", "purple")
+# print(colors[:3])
+# print(colors[-2:])
 
 # Question 4:
 # unpacking a tuple
-person = ("John", 25, "London")
-name, age, city = person
-print(name)
-print(age)
-print(city)
+
+# main tuple variable
+# person = ("John", 25, "London")
+
+# unpacked variables
+# name, age, city = person
+
+# print(person)
+# 
+# print()
+# 
+# print("------Unpacked--------")
+# print(name)
+# print(age)
+# print(city)
 
 
-x, y, z = 1, 2, 13
+# question 5:
+animals = ("dog", "cat", "lion")
+
+# convert the tuple to a list
+animals_list = list(animals)
+animals_list.append("tiger")
+
+# convert the list back to a tuple
+animals = tuple(animals_list)
+
+print(animals)
--- example.py
+++ example.py
@@ -1,10 +1,28 @@
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
-weight=[12.3, 45.6, 78.9, 23.4, 56.7]
-height=[1.2, 1.5, 1.8, 1.3, 1.6]
+# weight=[12.3, 45.6, 78.9, 23.4, 56.7]
+# height=[1.2, 1.5, 1.8, 1.3, 1.6]
 
-plt.scatter(weight, height)
-plt.xlabel("Weight (kg)")
-plt.ylabel("Height (m)")
-plt.title("Weight vs Height")
-plt.show()
+# plt.scatter(weight, height)
+# plt.xlabel("Weight (kg)")
+# plt.ylabel("Height (m)")
+# plt.title("Weight vs Height")
+# plt.show()
+
+
+# name="mercy"
+# age=26
+# weight=55
+# location="london"
+# height=5.5
+
+# multiple variable assignment /PACKING
+name, age, weight, location, height = "mercy", 26, 55, "london", 5.5
+
+print(f"My name is {name}, I am {age} years old, I weigh {weight} kg, I live in {location}, and I am {height} feet tall.")
+
+
+# UNPACKING
+choice=("red", "blue", "green")
+color1, color2, color3 = choice
+print(color1)
