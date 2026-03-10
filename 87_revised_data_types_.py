ca6160570c45fedfa814f35c303fdeb74e6ebb71
Samson Oladimeji Odunaiya <102425294+Samklugh@users.noreply.github.com>
2026-03-10 00:33:16 +0000

    data types summary

---
+++ 87_revised_data_types_.py
@@ -0,0 +1,53 @@
+# PYTHON DATA TYPES
+
+# ----------------------------------------------------
+#                       strings
+# ----------------------------------------------------
+# 1. string: alphabet or anything in quatation marks.
+
+example_string = "This is a string 1243!@#"
+
+# ----------------------------------------------------
+#                       numbers
+# ----------------------------------------------------
+
+# Numbers can be of different types:
+# 2. integer
+
+example_integer = 1234567890
+
+
+# 3. float
+example_float= 3.14159
+
+
+# ----------------------------------------------------
+#                       booleans
+# ----------------------------------------------------
+# 4. booleans: can be either True or False
+
+example_boolean = True
+
+
+# ----------------------------------------------------
+#            collections or Objects/CLASSES
+# ----------------------------------------------------
+# 5. list: a collection of items that can be of different data types, ordered and CHANGEABLE.
+
+example_list = [1, 2, 3, "four", "five", 6.0, True]
+
+
+
+
+# 6. tuple: a collection of items that can be of different data types, ordered and UNCHANGEABLE.
+example_tuple = (1, 2, 3, "four", "five", 6.0, True)
+
+
+
+# 7. set: a collection of unique items that can be of different data types, UNORDERED and UNINDEXED.
+example_set = {1, 2, 3, "four", "five", 6.0, True}
+
+
+
+# 8. dictionary: a collection of key-value pairs, UNORDERED, CHANGEABLE, and INDEXED.
+example_dictionary = {"one": 1, "two": 2, "three": 3, "four": 4}
