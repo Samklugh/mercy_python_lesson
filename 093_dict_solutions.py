a6b392cb933f654bded98ce765af9d7720e19bb2
Samson Oladimeji Odunaiya <102425294+Samklugh@users.noreply.github.com>
2026-03-25 19:43:52 +0000

    solutions

---
+++ 093_dict_solutions.py
@@ -0,0 +1,16 @@
+# ******************
+#    Exercise 1
+# ******************
+
+
+# student= {"name": "John Doe", "age": 25, "courses": ["Math", "CompSci"]}
+# 
+# print(student)
+
+
+
+# ******************
+#    Exercise 2
+# ******************
+
+print(student["name"])
